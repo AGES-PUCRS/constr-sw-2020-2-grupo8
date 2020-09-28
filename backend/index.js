a346b1aca6a98aff51bd0ad255079efdd476ef0d
Gustavo Possebon <gustavo.possebon@acad.pucrs.br>
2020-09-28 20:48:24 -0300

    Mais coisas

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,16 @@
 /* Back-end para o trabalho da cadeira de Construção de Software */
 
 /* swagger-ui is a traditional npm module intended for use in single-page applications that are capable of resolving dependencies (via Webpack, Browserify, etc). */
-const swaggerUI = require('swagger-ui')
+const swaggerUI = require("swagger-ui");
 
-const openApiDocumentation = require('./openApiDocumentation');
+const openApiDocumentation = require("./openApiDocumentation");
 /* Hapi is a simple to use configuration-centric framework with built-in support for input validation, 
 caching, authentication, and other essential facilities for building web and services applications. */
-const hapi = require("hapi"); 
+const hapi = require("hapi");
 /* Joi lets you describe your data using a simple, intuitive, and readable language. */
 const joi = require("joi");
 /* Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports both promises and callbacks. */
-const mongoose = require("mongoose"); 
-
-
+const mongoose = require("mongoose");
 
 const server = new hapi.Server({
   host: "localhost/constr-sw-2020-2-grupo8",
@@ -20,7 +18,7 @@ const server = new hapi.Server({
   routes: { cors: { origin: ["*"] } },
 });
 
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 /* MongoDb connection */
 mongoose.connect("mongodb://localhost/constr-sw-2020-2-grupo8", {
@@ -33,44 +31,45 @@ const DisciplineModel = mongoose.model("discipline", {
   nome: String,
   codigo: Number,
   creditos: Number,
-  objetivos: String, 
+  objetivos: String,
   ementa: String,
   bibliografia: Array,
-  criado: Date.now
+  criado: Date.now,
 });
 
 //Buscar todas as disciplinas que estão cadastradas
 server.route({
-    method: "GET",
-    path: "/disciplines",
-    handler: async (request, resp) => {
-      try {
-        var disciplines = await DisciplineModel.find().exec();
-        var count = await DisciplineModel.countDocuments().exec();
-        var data = {
-          status: "Success",
-          message: "Disciplines retrieved successfully",
-          disciplines: disciplines,
-          count: count
-        };
-        return resp.response(data);
-      } catch (error) {
-        return resp.response(error).code(500);
-      }
+  method: "GET",
+  path: "/disciplinas",
+  handler: async (request, resp) => {
+    try {
+      let disciplines = await DisciplineModel.find().exec();
+      let count = await DisciplineModel.countDocuments().exec();
+      let data = {
+        status: "Sucesso",
+        message: "Disciplinas resgatadas com sucesso!",
+        disciplines: disciplines,
+        count: count,
+      };
+      return resp.response(data).code(200);
+    } catch (error) {
+      let errorResponse = error;
+      return resp.response(errorResponse).code(500);
     }
+  },
 });
 
 //Registar uma nova turma
 server.route({
   method: "POST",
-  path: "/registerDiscipline",
+  path: "/disciplinas",
   options: {
     validate: {
       payload: {
         nome: joi.string().required(),
         codigo: joi.number().required(),
         creditos: joi.string().required(),
-        objetivos: joi.string().required(), 
+        objetivos: joi.string().required(),
         ementa: joi.string().required(),
         bibliografia: joi.array().required(),
       },
@@ -83,12 +82,63 @@ server.route({
   },
   handler: async (request, resp) => {
     try {
+      let codigoRequest = request.payload.codigo;
+      let findOne = DisciplineModel.findOne({ codigo: codigoRequest }).exec();
+      if (findOne) {
+        let data = {
+          status: "Falha",
+          message: "Objeto ja existente",
+          discipline: findOne,
+        };
+        return resp.response(data).code(302);
+      }
       let discipline = new DisciplineModel(request.payload);
-      await user.save();
+      await discipline.save();
       let data = {
-        message: "Discipline created!",
+        status: "Sucesso",
+        message: "Disciplina criada!",
+        discipline: discipline,
       };
-      return resp.response(data);
+      return resp.response(data).code(201);
+    } catch (error) {
+      return resp.response(error).code(500);
+    }
+  },
+});
+
+//Buscar uma nova disciplina
+server.route({
+  method: "GET",
+  path: "/disciplinas/{id}",
+  options: {
+    validate: {
+      payload: {
+        id: joi.string().required(),
+      },
+      failAction: (request, resp, error) => {
+        return error.isJoi
+          ? resp.response(error.details[0]).takeover()
+          : resp.response(error).takeover();
+      },
+    },
+  },
+  handler: async (request, resp) => {
+    try {
+      let findOne = DisciplineModel.findOne({ _id: request.params.id }).exec();
+      if (findOne) {
+        let data = {
+          status: "Sucesso",
+          message: "Objeto encontrado",
+          discipline: findOne,
+        };
+        return resp.response(data).code(302);
+      } else {
+        let data = {
+          status: "Falha",
+          message: "Objeto não encontrado encontrado",
+        };
+        return resp.response(data).code(404);
+      }
     } catch (error) {
       return resp.response(error).code(500);
     }
