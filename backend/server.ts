b40d0c01c79ec41266a84fccac02e6a12bd4da3e
Gustavo Possebon <gustavo.possebon@acad.pucrs.br>
2020-10-22 11:29:23 -0300

    Fix docker and server

--- backend/server.ts
+++ backend/server.ts
@@ -18,13 +18,12 @@ const port = 3333;
 const url = "mongodb://localhost:27017/disciplinas";
 
 mongoose.connect(url, {
-  auth: {
-    authSource: "admin",
-  },
-  user: "user",
-  pass: "pass",
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	authSource: "admin",
+	user: "root",
+	pass: "password",
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
 //Get the default connection
