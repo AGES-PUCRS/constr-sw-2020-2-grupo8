0f267e794010b0637c50901e37300e5853f617ee
Carlos Henrique <carloshsc1998@gmail.com>
2020-11-22 00:10:19 -0300

    Pegando os dados da API

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,41 +1,45 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import { HttpClient } from '@angular/common/http';
+import {MatIcon} from "@angular/material/icon";
 
-export interface PeriodicElement {
-  professor: string;
-  id: number;
-  formacao: string;
-  turmas: number;
-  turno: string;
+export interface Content {
 
+  cdMatricula: string;
+  email: string;
+  escola: string;
+  id: string;
+  isAtivo: true;
+  nome: string;
+  numTelefone: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 17120004, professor: 'Ana Paula', formacao: 'Tecnologia da Informação', turmas: 8, turno:'Manhã' },
-  {id: 17120005, professor: 'Eduardo', formacao: 'Ciência da Computação', turmas: 7, turno:'Manhã, Noite'},
-  {id: 17120006, professor: 'Silvia', formacao: 'Ciência da Computação', turmas: 10, turno:'Noite'},
-  {id: 17120007, professor: 'Alessandra', formacao: 'Sistemas de Informação', turmas: 12, turno:'Tarde, Noite'},
-  {id: 17120008, professor: 'Isabel', formacao: 'Análise de Dados', turmas: 14, turno:'Manhã, Tarde'},
-  {id: 17120009, professor: 'Sergio', formacao: 'Engenharia da Computação', turmas: 6, turno:'Manhã, Tarde, Noite'},
-
-];
-
-/**
- * @title Table with sorting
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'professor', 'formacao', 'turmas', 'turno'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  @ViewChild(MatSort) sort: MatSort;
+export class AppComponent {
+  displayedColumns = ["cdMatricula", "nome", "email", "numTelefone", "escola", "edit", "delete"];
+
+
+  constructor(private http: HttpClient) {
+  }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+    this.http.get<Content>('http://ec2-3-91-232-225.compute-1.amazonaws.com:3333/professores')
+        .subscribe((data: Content[] | any) => {
+          console.log(data);
+          this.dataSource = new MatTableDataSource(data);
+        })
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
   }
 }
